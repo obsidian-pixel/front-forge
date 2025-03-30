003cf7e853a57ea5e980457b48882519603b9ecf
obsidian-pixel <cornelius.labuschagne@outlook.com>
2025-03-30 05:15:00 +0200

    img path override - test 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,7 @@ export default function Home() {
                 <Image
                   width={100}
                   height={100}
+                  overrideSrc="./public/x-com-logo.avif"
                   src={"/x-com-logo.avif"}
                   alt={""}
                 ></Image>
